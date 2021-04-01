7ec47188663605de7312b699477969463b29b186
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-04-01 15:22:29 +0100

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -19,8 +19,8 @@ if(Auth::check()) $user = Auth::user();
 				<div class="row text-left">
 					<div class="col-sm-6">
 						<div class="page_404_content">
-							<h1>Not Found - We Searched Very Hard</h1>
-							<h3>The page you requested cannot be found.</h3>
+							<h1>404</h1>
+							<h3>We searched very hard however the page you requested cannot be found.</h3>
 						</div>
 						<div class="404_btn">
 							<a href="{{url('/')}}" class="btn border-btn"><i class="fa fa-arrow-circle-o-left"></i> Back To Home</a>
