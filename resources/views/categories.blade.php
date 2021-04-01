9d226f54d44b67ad5b16d411c26ec4fa74ff770b
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-04-01 16:02:56 +0100

    Update categories.blade.php

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -6,76 +6,50 @@ $pcClass = "";
 @extends('layout')
 
 @section('content')
-<section class="mt-md-10 pt-md-3 mt-9">
-                        <h2 class="title">Available Categories</h2>
-						<?php
-						 if(count($c) > 0)
-						 {
-						?>
-						
-						<div class="owl-carousel owl-theme owl-loaded owl-drag" data-owl-options="{
-                            'nav': false,
-                            'dots': true,
-                            'items': 4,
-                            'margin':  20,
-                            'loop': false,
-                            'responsive': {
-                                '0': {
-                                    'items': 1 
-                                },
-                                '576': {
-                                    'items': 2
-                                },
-                                '768': {
-                                    'items': 3
-                                },
-                                '992': {
-                                    'items': 4,
-                                    'dots': false
-                                }
-                            }
-                        }">
-                            
-                        <div class="owl-stage-outer">
-						   <div class="owl-stage" style="transform: translate3d(0px, 0px, 0px); transition: all 0s ease 0s; width: 1200px;">
-						     <?php
-							   foreach($c as $cc)
-						       {
-							     $cu = url('category')."?xf=".$cc['category']; 
-							     $imgs = $cc['image'];
-							     $parent = $cc['parent'];
-							     $pc = $cc['product_count'];
-							     $pcText = $pc == 1 ? "Product" : "Products";
-							 ?>
-							 <div class="owl-item active" style="width: 280px; margin-right: 20px;">
-							   <div class="category category-ellipse category-absolute overlay-zoom">
-                                <a href="{{$cu}}">
-                                    <figure class="category-media">
-                                        <img src="{{$imgs[0]}}" alt="category" width="280" height="280">
-                                    </figure>
-                                </a>
-                                <div class="category-content">
-                                    <h4 class="category-name"><a href="{{$cu}}">{{ucwords($cc['name'])}}</a></h4>
-                                    <span class="category-count">
-                                        <span>{{$pc}}</span> {{$pcText}}
-                                    </span>
-                                </div>
-                               </div>
-							</div>
-							<?php
-						   }
-							?>
-						   </div>
-						</div>
-						<div class="owl-nav disabled">
-						  <button type="button" role="presentation" class="owl-prev"><i class="d-icon-angle-left"></i></button>
-						  <button type="button" role="presentation" class="owl-next"><i class="d-icon-angle-right"></i></button>
-						</div>
-						<div class="owl-dots disabled"></div>
-					</div>
-						 
-                          <?php
-						 }
-						  ?>
-                    </section>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <?php
+	 if(count($c) > 0)
+	 {
+  ?>
+  <div class="carousel-inner">
+  <?php
+	 for($i = 0; $i < count($c); $i++)
+     {
+      $cc = $c[$i];
+	  $cu = url('category')."?xf=".$cc['category']; 
+	  $imgs = $cc['image'];
+	  $parent = $cc['parent'];
+	  $pc = $cc['product_count'];
+	  $pcText = $pc == 1 ? "Product" : "Products";
+   ?>
+    <div class="carousel-item active">
+      <div class="d-block w-100">
+         <h4 class="category-name"><a href="{{$cu}}">{{ucwords($cc['name'])}}</a></h4>
+         <span class="category-count">
+            <span>{{$pc}}</span> {{$pcText}}
+         </span>
+      </div>
+    </div>
+   <?php
+    }
+   ?>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  <?php
+  }
+  ?>
+</div>
 @stop
