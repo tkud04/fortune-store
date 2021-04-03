c5cb5f14b10bd8a7023037ce54d170b7177b4afc
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-04-03 15:27:36 +0100

    Update product.blade.php

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -39,7 +39,7 @@ $pcClass = "";
  ?>
  	
  $(document).ready(() => {
- 	alert("imgs.length: " + imgs.length);
+ 	//alert("imgs.length: " + imgs.length);
  	showImage(0);
  });
  </script>
@@ -51,7 +51,7 @@ $pcClass = "";
 				<!-- Product Details Image -->
 				<div class="col-md-6 col-xs-12">
 					<div class="pd_img fix">
-						  <a id="va" href="javascript:void(0)"><img id="vi" src="" alt="{{$displayName}}"/></a>
+						  <a id="va" data-img="" href="javascript:void(0)"><img id="vi" src="" alt="{{$displayName}}"/></a>
 					</div>
 					<div class="pd_btn fix">
 							<a href="javascript:void(0)" id="vprev" class="btn btn-default acc_btn btn_icn"><i class="fa fa-chevron-left"></i></a>
@@ -65,8 +65,8 @@ $pcClass = "";
 						<div class="pd_price_dtls fix">
 							<!-- Product Price -->
 							<div class="pd_price">
-								<span class="new">{{number_format($amount,2)}}</span>
-								<span class="old">(60.00)</span>
+								<span class="new">&#8358;{{number_format($amount,2)}}</span>
+								
 							</div>
 							<!-- Product Ratting -->
 							<div class="pd_ratng">
@@ -81,45 +81,31 @@ $pcClass = "";
 						</div>
 						<div class="pd_text">
 							<h4>overview:</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tem portul indunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud  exercitation ullamco laboris nisi ut aliquip.</p>
+							<p> SKU: <span class="product-sku">{{$product['sku']}}</span> </p>
 						</div>
-						<div class="pd_img_size fix">
-							<h4>size:</h4>
-							<a href="#">s</a>
-							<a href="#">m</a>
-							<a href="#">l</a>
-							<a href="#">xl</a>
-							<a href="#">xxl</a>
-						</div>
+						
 						<div class="pd_clr_qntty_dtls fix">
-							<div class="pd_clr">
-								<h4>color:</h4>
-								<a href="#" class="active" style="background: #ffac9a;">color 1</a>
-								<a href="#" style="background: #ddd;">color 2</a>
-								<a href="#" style="background: #000000;">color 3</a>
-							</div>
+							
 							<div class="pd_qntty_area">
 								<h4>quantity:</h4>
 								<div class="pd_qty fix">
-									<input value="1" name="qttybutton" class="cart-plus-minus-box" type="number">
+									<input value="1" name="qttybutton" id="product-qty" class="cart-plus-minus-box" type="number">
 								</div>
 							</div>
 						</div>
 						<!-- Product Action -->
 						<div class="pd_btn fix">
-							<a class="btn btn-default acc_btn">add to bag</a>
+							<a class="btn btn-default acc_btn" href="javascript:void(0)" id="product-add-to-cart-btn" >add to bag</a>
 							<a class="btn btn-default acc_btn btn_icn"><i class="fa fa-heart"></i></a>
-							<a class="btn btn-default acc_btn btn_icn"><i class="fa fa-refresh"></i></a>
+							
 						</div>
 						<div class="pd_share_area fix">
 							<h4>share this on:</h4>
 							<div class="pd_social_icon">
 								<a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
 								<a class="twitter" href="#"><i class="fa fa-twitter"></i></a>
-								<a class="vimeo" href="#"><i class="fa fa-vimeo"></i></a>
-								<a class="google_plus" href="#"><i class="fa fa-google-plus"></i></a>
-								<a class="tumblr" href="#"><i class="fa fa-tumblr"></i></a>
-								<a class="pinterest" href="#"><i class="fa fa-pinterest"></i></a>
+								<a class="instagram" href="#"><i class="fa fa-instagram"></i></a>
+								
 							</div>
 						</div>
 					</div>
@@ -133,9 +119,7 @@ $pcClass = "";
 						  <li>
 							<a class="active" href="#description" role="tab" data-toggle="tab">Description</a>
 						  </li>
-						  <li>
-							<a href="#information" role="tab" data-toggle="tab">Information</a>
-						  </li>
+						  
 						  <li>
 							<a href="#reviews" role="tab" data-toggle="tab">Reviews</a>
 						  </li>
@@ -144,26 +128,10 @@ $pcClass = "";
 						<!-- Tab panes -->
 						<div class="tab-content">
 							<div role="tabpanel" class="tab-pane fade show active" id="description">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
-								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-								dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-								Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-								anim id est laborum.</p>
-								<ul>
-									<li>Lorem ipsum dolor sit amet, consectetur product</li>
-									<li>Duis aute irure dolor in reprehenderit in voluptate velit esse</li>
-									<li>Excepteur sinted occaecat cupidatat non proident products</li>
-									<li>Voluptate velit esse cillum.</li>
-								</ul>					  
+								{!! $description !!}					  
 							</div>
 
-							<div role="tabpanel" class="tab-pane fade" id="information">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
-								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-								dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>										  
-							</div>
+							
 
 								<div role="tabpanel" class="tab-pane fade" id="reviews">
 									<div class="pda_rtng_area fix">
