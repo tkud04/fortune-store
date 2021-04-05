1b7fa967032501729d105043110072828de780c3
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-04-05 14:54:10 +0100

    Update cart.blade.php

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -7,26 +7,27 @@ $pcClass = "";
 
 @section('content')
 
-
 <script>
 let cart = [];
 </script>
-				@include('checkout-header',['number' => 1])
-				<div class="container mt-8 mb-4">
-					<div class="row gutter-lg">
-						<div class="col-lg-8 col-md-12">
-							<table id="cart-table" class="shop-table cart-table mt-2">
+					<!-- Cart Page -->
+		<div class="cart_page_area">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="cart_table_area table-responsive">
+							<table id="cart-table" class="table cart_prdct_table text-center">
 								<thead>
 									<tr>
-										<th><span>Product</span></th>
-										<th></th>
-										<th><span>Price</span></th>
-										<th><span>quantity</span></th>
-										<th>Subtotal</th>
+										<th class="cpt_img">Product</th>
+										<th class="cpt_q">quantity</th>
+										<th class="cpt_p">price</th>
+										<th class="cpt_t">total</th>
+										<th class="cpt_r">remove</th>
 									</tr>
 								</thead>
 								<tbody>
-								<?php
+									<?php
 								$cc = (isset($cart)) ? count($cart) : 0;
 								   $subtotal = 0;
 				                   for($a = 0; $a < $cc; $a++)
@@ -40,95 +41,66 @@ let cart = [];
 									 $uu = url('product')."?xf=".$xf;
 									 $ru = url('remove-from-cart')."?xf=".$xf;
 				                 ?>
-								   <script>
-								    // cart.push({xf: "{{$qty}}",qty: "{{$qty}}"});
-								   </script>
 									<tr>
-										<td class="product-thumbnail">
-											<figure>
-												<a href="{{$uu}}">
-													<img src="{{$imgs[0]}}" width="100" height="100" alt="{{$item['name']}}">
-												</a>
-												<a href="{{$ru}}" class="product-remove" title="Remove this product">
-													<i class="fas fa-times"></i>
-												</a>
-											</figure>
-										</td>
-										<td class="product-name">
-											<div class="product-name-section">
-												<a href="{{$uu}}">{{$item['name']}}</a>
+										<td>
+                                          <a href="{{$uu}}" class="cp_img"><img src="{{$imgs[0]}}" alt="{{$item['name']}}" /></a>
+                                          <a href="{{$uu}}" class="cp_title">{{$item['name']}}</a>
+                                          <a href="{{$uu}}" class="cp_title">{{$item['name']}}</a>
+                                        </td>
+										<td></td>
+										<td>										
+											<div class="cp_quntty">																			
+												<input name="quantity" value="{{$qty}}" data-val="{{$qty}}" data-xf="{{$xf}}" size="2" type="number">													
 											</div>
 										</td>
-										<td class="product-subtotal">
-											<span class="amount">&#0163;{{number_format($itemAmount,2)}}</span>
-										</td>
-										<td class="product-quantity">
-											<div class="input-group">
-												<button class="quantity-minus d-icon-minus"></button>
-												<input class="quantity form-control product-qty" type="number" value="{{$qty}}" data-val="{{$qty}}" data-xf="{{$xf}}" min="1" max="1000000">
-												<button class="quantity-plus d-icon-plus"></button>
-											</div>
-										</td>
-										<td class="product-price">
-											<span class="amount">&#0163;{{number_format($itemAmount * $qty,2)}}</span>
-										</td>
+										<td><p class="cp_price">&#8358;{{number_format($itemAmount,2)}}</p></td>
+										<td><p class="cpp_total">&#8358;{{number_format($itemAmount * $qty,2)}}</p></td>
+										<td><a href="{{$ru}}" title="Remove this product" class="btn btn-default cp_remove"><i class="fa fa-trash"></i></a></td>
 									</tr>
 									<?php
 								   }
 									?>
 									
+									
 								</tbody>
 							</table>
-							<div class="cart-actions mb-6 pt-6">
-								<div class="coupon">
-									<input type="text" name="coupon_code" class="input-text form-control" id="coupon_code" value="" placeholder="Coupon code">
-									<button type="submit" class="btn btn-md">Apply Coupon</button>
-								</div>
-								<a href="javascript:void(0)" id="update-cart-btn" class="btn btn-md btn-icon-left"><i class="d-icon-refresh"></i>Update Cart</a>
-							</div>
 						</div>
-						<aside class="col-lg-4 sticky-sidebar-wrapper">
-							<div class="sticky-sidebar" data-sticky-options="{'bottom': 20}">
-								<div class="summary mb-4">
-									<h3 class="summary-title text-left">Cart Totals</h3>
-									<table class="shipping">
-										<tbody><tr class="summary-subtotal">
-											<td>
-												<h4 class="summary-subtitle">Subtotal</h4>
-											</td>
-											<td>
-												<p class="summary-subtotal-price">&#0163;{{number_format($subtotal,2)}}</p>
-											</td>												
-										</tr>
-										<tr class="sumnary-shipping shipping-row-last">
-											<td colspan="2">
-												<h4 class="summary-subtitle">Shipping</h4>
-												<ul>
-													<li>
-														<div class="custom-radio">
-															<input type="radio" id="free-shipping" name="shipping" class="custom-control-input" checked="checked">
-															<label class="custom-control-label" for="free-shipping">Free
-																Shipping</label>
-														</div>
-													</li>
-												</ul>
-											</td>
-										</tr>
-									</tbody></table>
-									<table>
-										<tbody><tr class="summary-subtotal">
-											<td>
-												<h4 class="summary-subtitle">Total</h4>
-											</td>
-											<td>
-												<p class="summary-total-price">&#0163;{{number_format($subtotal,2)}}</p>
-											</td>												
-										</tr>
-									</tbody></table>
-									<a href="{{url('checkout')}}" class="btn btn-dark btn-checkout">Proceed to checkout</a>
-								</div>
-							</div>
-						</aside>
 					</div>
 				</div>
+				
+				<div class="row">
+					<div class="col-md-8 col-xs-12 cart-actions cart-button-cuppon">
+						<div class="row">
+							<div class="col-sm-7">
+								<div class="cart-action">
+									<a href="{{url('/')}}" class="btn border-btn">continiue shopping</a>
+									<a href="javascript:void(0)" id="update-cart-btn" class="btn border-btn">update shopping bag</a>
+								</div>
+							</div>
+							
+							<div class="col-sm-5">
+								<div class="cuppon-wrap">
+									<h4>Discount Code</h4>
+									<p>Enter your coupon code if you have</p>
+									<form action="#" class="cuppon-form">
+										<input type="text" />
+										<button class="btn border-btn">apply coupon</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-md-4 col-xs-12 cart-checkout-process text-right">
+						<div class="wrap">
+							<p><span>Subtotal</span><span>&#8358;{{number_format($subtotal,2)}}</span></p>
+							<h4><span>Total</span><span>&#8358;{{number_format($subtotal,2)}}</span></h4>
+							<a href="{{url('checkout')}}" class="btn border-btn">process to checkout</a>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
 @stop
